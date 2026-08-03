f8d66b7d2c49bcdfeac74ecca67a8a61a033ff47
Tonyjoo11 <hyungjoo.ree@gmail.com>
2026-08-03 14:54:52 +0900

    [chore] alembic init, ingredient migrated

--- app/models/ingredient.py
+++ app/models/ingredient.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import Boolean, DateTime, String
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.database import Base
@@ -9,9 +7,21 @@ from app.database import Base
 class Ingredient(Base):
     __tablename__ = "ingredient"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    deadline: Mapped[datetime|None] = mapped_column(DateTime)
-    category: Mapped[str|None] = mapped_column(String(120))
-    emoji: Mapped[str|None] = mapped_column(String(20))
-    starred: Mapped[bool] = mapped_column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+        unique=True,
+    )
+
+    category: Mapped[str | None] = mapped_column(
+        String(120),
+    )
+
+    emoji: Mapped[str | None] = mapped_column(
+        String(20),
+    )
